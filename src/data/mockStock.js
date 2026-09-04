a6e798de0738d2fab9e2b1eba04f960e67bda09d
Umugwaneza Aline <umugwanezaaline77@gmail.com>
2026-09-04 20:23:27 +0200

    data

---
+++ src/data/mockStock.js
@@ -0,0 +1,70 @@
+export const CHIVUCHA_MASTER_STOCK = [
+  {
+    id: "cat-dairy",
+    categoryName: "Dairy-Based Beverages",
+    products: [
+      { id: "p-db-1", name: "Shake n Sip", size: "500ml", details: "Strawberry, banana", qty: 250, price: 1200 },
+      { id: "p-db-2", name: "Shake n Sip", size: "250ml", details: "Strawberry, banana", qty: 180, price: 700 },
+      { id: "p-db-3", name: "Sorgo maheu", size: "500ml", details: "Ordinary", qty: 300, price: 600 },
+      { id: "p-db-4", name: "Amasipsip", size: "500ml", details: "strawberry, banana, milk butter", qty: 140, price: 800 },
+      { id: "p-db-5", name: "Milkit", size: "500ml", details: "vanilla, strawberry, chocolate, banana", qty: 210, price: 1100 },
+      { id: "p-db-6", name: "Milk creambell scht", size: "475ml", details: "225ml packs", qty: 90, price: 1000 },
+      { id: "p-db-7", name: "Milk dairy gold scht", size: "475ml", details: "225ml packs", qty: 85, price: 1200 },
+      { id: "p-db-8", name: "Finta milk bx", size: "500ml", details: "225ml packs", qty: 140, price: 1500 },
+      { id: "p-db-9", name: "Fruiticana smoothie", size: "Standard", details: "Orange, water melon, kiwi", qty: 175, price: 1300 }
+    ]
+  },
+  {
+    id: "cat-soft",
+    categoryName: "Soft Drinks",
+    products: [
+      { id: "p-sd-1", name: "Coca cola ordinary", size: "500ml", details: "350ml packs", qty: 500, price: 800 },
+      { id: "p-sd-2", name: "Fanta ordinary", size: "500ml", details: "350ml packs", qty: 480, price: 800 },
+      { id: "p-sd-3", name: "Zero coke", size: "500ml", details: "Sugar Free", qty: 190, price: 900 },
+      { id: "p-sd-4", name: "Sprite", size: "500ml", details: "Clear Lemon", qty: 340, price: 800 },
+      { id: "p-sd-5", name: "Tonic water", size: "350ml", details: "Mixer", qty: 120, price: 1000 },
+      { id: "p-sd-6", name: "Tango pina", size: "Standard", details: "Pineapple", qty: 150, price: 700 },
+      { id: "p-sd-7", name: "Mojo", size: "Standard", details: "lemon, orange, ginger, blue", qty: 240, price: 800 },
+      { id: "p-sd-8", name: "Minute maid", size: "500ml", details: "Tropical, mango", qty: 280, price: 1200 },
+      { id: "p-sd-9", name: "Fruiticana", size: "Standard", details: "Mixed fruit, mixed berry, mango, guava", qty: 390, price: 1000 },
+      { id: "p-sd-10", name: "Fresh up", size: "Standard", details: "mango, guava, apple", qty: 220, price: 1100 },
+      { id: "p-sd-11", name: "Mirinda", size: "500ml", details: "Orange, grape", qty: 310, price: 800 },
+      { id: "p-sd-12", name: "Vitana", size: "Standard", details: "mango, pineapple, orange", qty: 180, price: 900 },
+      { id: "p-sd-13", name: "Mazoe jolly juice", size: "2L", details: "Concentrate", qty: 95, price: 4500 },
+      { id: "p-sd-14", name: "Mazoe drink", size: "Standard", details: "cream soda, raspberry", qty: 140, price: 2000 },
+      { id: "p-sd-15", name: "Pure juice", size: "5L", details: "Orange, pineapple", qty: 60, price: 8500 },
+      { id: "p-sd-16", name: "Coolsprash", size: "5L", details: "2L, pineapple, orange", qty: 110, price: 7000 },
+      { id: "p-sd-17", name: "Tabby juice", size: "5L", details: "Orange, pineapple", qty: 45, price: 7500 },
+      { id: "p-sd-18", name: "Yess fruita", size: "330ml", details: "mango, guava, pineapple", qty: 260, price: 700 },
+      { id: "p-sd-19", name: "Go fresh", size: "1.5L", details: "pineapple, orange, peach", qty: 130, price: 1800 },
+      { id: "p-sd-20", name: "Just", size: "2L", details: "Orange, pineapple, granadilla", qty: 120, price: 2400 },
+      { id: "p-sd-21", name: "Yess drink ordinary", size: "500ml", details: "1*20, 1*12 Orange, pineapple", qty: 190, price: 800 },
+      { id: "p-sd-22", name: "Fruitop", size: "1.5L", details: "pineapple, orange, granadilla", qty: 145, price: 1700 },
+      { id: "p-sd-23", name: "Brothers", size: "500ml", details: "Apple, grape", qty: 160, price: 1200 }
+    ]
+  },
+  {
+    id: "cat-water",
+    categoryName: "Water Grid",
+    products: [
+      { id: "p-w-1", name: "Aquasavana", size: "750ml", details: "500ml packs", qty: 300, price: 600 },
+      { id: "p-w-2", name: "Vatra", size: "750ml", details: "500ml packs", qty: 200, price: 700 },
+      { id: "p-w-3", name: "Aquaclear", size: "1L", details: "750ml, 500ml packs", qty: 220, price: 800 },
+      { id: "p-w-4", name: "Zamdrop", size: "1.5L", details: "600ml packs", qty: 150, price: 1000 }
+    ]
+  },
+  {
+    id: "cat-energy",
+    categoryName: "Energy Drinks Engine",
+    products: [
+      { id: "p-ed-1", name: "Mojo energy drink", size: "Standard", details: "Active Booster", qty: 240, price: 1000 },
+      { id: "p-ed-2", name: "Yess energy", size: "Standard", details: "Classic Power", qty: 190, price: 1000 },
+      { id: "p-ed-3", name: "Kung fu", size: "500ml", details: "Ordinary, mixed berry, dry lemon", qty: 320, price: 1200 },
+      { id: "p-ed-4", name: "Kung fu ordinary", size: "330ml", details: "Standard Shot", qty: 210, price: 800 },
+      { id: "p-ed-5", name: "Hardrock", size: "330ml", details: "pineapple, energy", qty: 175, price: 900 },
+      { id: "p-ed-6", name: "Power plus", size: "300ml", details: "Extra Stamina", qty: 140, price: 1100 },
+      { id: "p-ed-7", name: "Power", size: "250ml", details: "Compact Pack", qty: 160, price: 900 },
+      { id: "p-ed-8", name: "Tangawizi", size: "Standard", details: "Spiced Booster", qty: 200, price: 800 }
+    ]
+  }
+];
